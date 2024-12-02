7a279ed9cf7c9ae42e22d65db8df9e900973bcc9
yaozhong <yun.zhongyue@163.com>
2024-12-02 16:59:20 +0800

    update auth logic

--- wxcloudrun/auth/views.py
+++ wxcloudrun/auth/views.py
@@ -29,21 +29,7 @@ def login():
     user = User.query.filter_by(openid=openid).first()
     
     if not user:
-        # 用户不存在，创建未授权用户
-        user = User(
-            openid=openid,
-            nickname=data.get('nickname'),
-            avatar_url=data.get('avatar_url'),
-            is_authorized=False
-        )
-        db.session.add(user)
-        db.session.commit()
         return jsonify({'code': 1, 'msg': '用户未授权'})
-    else:
-        # 更新现有用户信息
-        user.nickname = data.get('nickname')
-        user.avatar_url = data.get('avatar_url')
-        db.session.commit()
     
     if not user.is_authorized:
         return jsonify({'code': 1, 'msg': '用户未授权'})
@@ -64,67 +50,42 @@ def authorize():
     data = request.get_json()
     code = data.get('code')
     
-    # 添加日志记录
-    print(f"收到授权请求，code: {code}")
-    
     # 通过code获取openid
     appid = config.WECHAT_APPID
     secret = config.WECHAT_SECRET
     url = f'https://api.weixin.qq.com/sns/jscode2session?appid={appid}&secret={secret}&js_code={code}&grant_type=authorization_code'
     
-    # 添加环境变量检查
-    if not appid or not secret:
-        print("微信配置缺失：", {"appid": appid, "secret": bool(secret)})
-        return jsonify({'code': -1, 'msg': '服务器配置错误'})
-    
     resp = requests.get(url)
     result = resp.json()
-    # 添加微信API返回结果日志
-    print("微信API返回：", result)
-    
     openid = result.get('openid')
+    
     if not openid:
-        error_msg = f"微信授权失败：{result.get('errmsg', '未知错误')}"
-        print(error_msg)
-        return jsonify({'code': -1, 'msg': error_msg})
+        return jsonify({'code': -1, 'msg': '授权失败'})
     
     # 查找或创建用户
     user = User.query.filter_by(openid=openid).first()
-    if user:
-        if user.is_authorized:
-            # 如果用户已授权,直接返回用户信息
-            return jsonify({
-                'code': 0,
-                'data': {
-                    'userInfo': {
-                        'nickname': user.nickname,
-                        'avatarUrl': user.avatar_url
-                    }
+    if user and user.is_authorized:
+        # 如果用户已授权,返回用户信息
+        return jsonify({
+            'code': 0,
+            'data': {
+                'userInfo': {
+                    'nickname': user.nickname,
+                    'avatarUrl': user.avatar_url
                 }
-            })
-        # 更新用户信息和授权状态
-        user.nickname = data.get('nickname')
-        user.avatar_url = data.get('avatar_url')
-        user.is_authorized = True
-    else:
-        # 创建新的授权用户
-        user = User(
-            openid=openid,
-            nickname=data.get('nickname'),
-            avatar_url=data.get('avatar_url'),
-            is_authorized=True
-        )
+            }
+        })
+    
+    # 未授权用户或新用户,只需要设置授权标记
+    if not user:
+        user = User(openid=openid, is_authorized=True)
         db.session.add(user)
+    else:
+        user.is_authorized = True
     
     db.session.commit()
     
     return jsonify({
         'code': 0,
-        'msg': '授权成功',
-        'data': {
-            'userInfo': {
-                'nickname': user.nickname,
-                'avatarUrl': user.avatar_url
-            }
-        }
+        'msg': '授权成功'
     }) 
